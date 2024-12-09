11f9cb49bf854e3ef2d06cdf13e78982069ccd16
gunal-01 <gunalprakash006@gmail.com>
2024-12-09 10:05:26 +0530

    newTaskAdded

--- Task-Tracker-App/src/scripts/app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("addTask").addEventListener("click", () => {
-    const taskTitle = document.getElementById("taskTitle").value;
-    const taskDescription = document.getElementById("taskDescription").value;
-
-    const li = document.createElement("li");
-    li.innerHTML = `<strong>${taskTitle}</strong>: ${taskDescription}`;
-    document.getElementById("taskList").appendChild(li);
-});
